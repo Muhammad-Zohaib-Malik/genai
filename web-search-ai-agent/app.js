cd1acf7abbd4d95d4324597b3009a71e935355cd
zohaib <muhammadzohaibmalik10@gmail.com>
2025-12-02 06:48:25 +0500

    refactor: streamline message handling and enhance web search functionality

--- web-search-ai-agent/app.js
+++ web-search-ai-agent/app.js
@@ -4,7 +4,70 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 const tvly = tavily({ apiKey: process.env.TRAVILY_API_KEY });
 
 async function main() {
-  const completion = await groq.chat.completions.create({
+  const messages=[
+      {
+        role: "system",
+        content: `you are smart assistant who answers the asked questions.
+        you have access to a tool called webSearch that helps you search the lastest information and real-time data from the web.
+        `,
+      },
+      {
+        role: "user",
+        content: `what was the release date and starting price of iphone16?`,
+      },
+    ]
+
+  const completions = await groq.chat.completions.create({
+    messages: messages,
+    model: "llama-3.3-70b-versatile",
+    temperature: 0,
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "webSearch",
+          description:
+            "search the lastest information and real-time data from the web",
+          parameters: {
+            type: "object",
+            properties: {
+              query: {
+                type: "string",
+                description: "The search query to perform search on",
+              },
+            },
+            required: ["query"],
+          },
+        },
+      },
+    ],
+    tool_choice: "auto",
+  });
+
+  messages.push(completions.choices[0].message)
+
+  const toolCalls = completions.choices[0].message.tool_calls;
+
+  if (!toolCalls) {
+    console.log(`Assistant ${completions.choices[0].message.content}`);
+    return;
+  }
+
+  for (const tool of toolCalls) {
+    const functionName = tool.function.name;
+    let args = tool.function.arguments;
+
+    if (functionName === "webSearch") {
+      const toolResult = await webSearch(JSON.parse(args));
+      console.log(`Tool result:`, toolResult);
+
+      messages.push({
+        tool_call_id:tool
+      })
+    }
+  }
+
+   const completions2 = await groq.chat.completions.create({
     messages: [
       {
         role: "system",
@@ -42,23 +105,8 @@ async function main() {
     tool_choice: "auto",
   });
 
-  const toolCalls = completion.choices[0].message.tool_calls;
 
-  if (!toolCalls) {
-    console.log(`Assistant ${completion.choices[0].message.content}`);
-    return;
-  }
 
-  for (const tool of toolCalls) {
-    console.log(`Tool call:`, tool);
-    const functionName = tool.function.name;
-    let args = tool.function.arguments;
-
-    if (functionName === "webSearch") {
-      const toolResult = await webSearch(JSON.parse(args));
-      console.log(`Tool result:`, toolResult);
-    }
-  }
 }
 
 main();
@@ -66,6 +114,9 @@ main();
 async function webSearch({ query }) {
   console.log(`Searching the web for query: ${query}`);
   const response = await tvly.search(query);
-  console.log("response", response);
-  return "iphone16 was realesed in september 2024 with a starting price of $999";
+  const finalResult = response.results
+    .map((result) => result.content)
+    .join("\n\n");
+  console.log(finalResult);
+  return finalResult;
 }
